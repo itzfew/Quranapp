2d214ba09b4552ab51d48e32babfb9ae120bed9c
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-09-03 21:41:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
- document.addEventListener('DOMContentLoaded', function() {
-    const queryParams = new URLSearchParams(window.location.search);
-    const surahNumber = queryParams.get('number');
-    const surahUrl = `https://raw.githubusercontent.com/penggguna/QuranJSON/master/surah/${surahNumber}.json`;
+document.addEventListener('DOMContentLoaded', function() {
+    const surahListElement = document.getElementById('surah-list');
+    const surahUrl = 'https://raw.githubusercontent.com/itzfew/Quran-Online/main/source/surah.json';
 
     fetch(surahUrl)
         .then(response => response.json())
         .then(data => {
-            const surahDetailsElement = document.getElementById('surah-details');
-
-            const surahTitle = document.createElement('h2');
-            surahTitle.classList.add('surah-title');
-            surahTitle.textContent = `${data.name} (${data.name_translations.en})`;
-            surahDetailsElement.appendChild(surahTitle);
-
-            data.verses.forEach(verse => {
-                const verseElement = document.createElement('div');
-                verseElement.classList.add('verse');
-
-                const verseNumber = document.createElement('span');
-                verseNumber.classList.add('verse-number');
-                verseNumber.textContent = verse.number;
-                verseElement.appendChild(verseNumber);
-
-                const verseText = document.createElement('div');
-                verseText.classList.add('verse-text');
-                verseText.textContent = verse.text;
-                verseElement.appendChild(verseText);
-
-                const translation = document.createElement('div');
-                translation.classList.add('translation');
-                translation.textContent = verse.translation_en;
-                verseElement.appendChild(translation);
-
-                surahDetailsElement.appendChild(verseElement);
+            data.forEach(surah => {
+                const listItem = document.createElement('li');
+                const link = document.createElement('a');
+                link.href = `surah.html?number=${surah.index}`;
+                link.textContent = `${surah.title} (${surah.titleAr})`;
+                listItem.appendChild(link);
+                surahListElement.appendChild(listItem);
             });
         })
-        .catch(error => console.error('Error fetching Surah details:', error));
+        .catch(error => console.error('Error fetching Surah data:', error));
 });
